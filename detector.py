e440512f61571dd8289313408c6c588f7b1e9fd5
Patrickharma <dharmapatrick88@gmail.com>
2024-05-22 14:05:10 +0900

    managed to separate the circle's color and detect each's coordinate

--- detector.py
+++ detector.py
@@ -10,6 +10,7 @@ def click_event(event, x, y, flags, param):
     if event == cv.EVENT_LBUTTONDOWN:  # Left button of the mouse is clicked
         print(x, ',', y)  # Print the coordinates of the point clicked
 
+
 def mouse_callback(name, img):
     cv.namedWindow(name)
     cv.setMouseCallback(name, click_event)
@@ -17,6 +18,7 @@ def mouse_callback(name, img):
     cv.imshow(name, img)
     cv.waitKey(0)
     cv.destroyAllWindows()
+
 
 def crop_img(img):
     """
@@ -31,6 +33,7 @@ def crop_img(img):
     text_part = img[328:514, 291:889]
     return board_part, text_part
 
+
 def read_image(path):
     # Expand user path if it starts with ~
     path = os.path.expanduser(path)
@@ -44,7 +47,92 @@ def read_image(path):
     # Blur using 3 * 3 kernel. 
     gray_blurred = cv.blur(gray, (3, 3))
 
-    return img, gray_blurred
+    return img
+
+
+def blur_image(img):
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    gray_blurred = cv.blur(gray, (3, 3))
+    return gray_blurred
+
+
+def hex_to_hsv(hex_color):
+    """
+    a tool to convert hex into its hsv value.
+    source: chatGPT
+    """
+    # Convert HEX to RGB
+    # remove the # from the beginning of the HEX string
+    hex_color = hex_color.lstrip('#')
+    # split the HEX string into its red, green, and blue
+    rgb_color = tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
+    # convert these components to integers
+    rgb_color = np.uint8([[rgb_color]])  # create an array for OpenCV
+
+    # Convert RGB to HSV
+    hsv_color = cv.cvtColor(rgb_color, cv.COLOR_RGB2HSV)
+    return hsv_color[0][0]
+
+
+def remove_background_noise(colored_board):
+    """
+    using cv2.bitwise_and() between mask and the image
+    to track a color, we define a mask in HSV color space using cv2.inRange()
+    passing lower and upper limits of color values in HSV
+    define a mask using np.zeros() 
+    and slicing the entries with white(255) for the region in the input img
+
+    use 3 masks:
+    red mask to extract only red circles
+    blue mask to extract only blue circles
+    green mask to extract only green circles.
+    """
+    # Convert BGR to HSV
+    # Hue: color type(ranging from 0 to 179)
+    # Saturation: the intensity or purity of the color(ranging 0-255)
+    # value: the brightness of the color (ranging 0-255)
+    hsv = cv.cvtColor(colored_board, cv.COLOR_BGR2HSV)
+
+    # HSV value for moonboard red #f44336
+    hsv_red = hex_to_hsv("#f44336")
+    # HSV value for moonboard blue #2961fe
+    hsv_blue = hex_to_hsv("#2961fe")
+    # HSV value for moonboard green #00c852
+    hsv_green = np.array([62, 139, 197])
+
+
+    # define range of red color in HSV
+    lower_red = np.array([hsv_red[0] - 10, hsv_red[1] - 40, hsv_red[2] - 40])
+    upper_red = np.array([hsv_red[0] + 10, hsv_red[1] + 40 ,hsv_red[2] + 40])
+
+    # define range of blue color in HSV
+    lower_blue = np.array([hsv_blue[0] - 10, hsv_blue[1] - 40, hsv_blue[2] - 40])
+    upper_blue = np.array([hsv_blue[0] + 10, hsv_blue[1] + 40, hsv_blue[2] + 40])
+
+    # define range of green color in HSV
+    lower_green = np.array([hsv_green[0] - 10, hsv_green[1] - 40, hsv_green[2] - 40])
+    upper_green = np.array([hsv_green[0] + 10, hsv_green[1] + 40, hsv_green[2] + 40]) 
+    
+    # create a mask
+    red_mask = cv.inRange(hsv, lower_red, upper_red)
+    blue_mask = cv.inRange(hsv, lower_blue, upper_blue)
+    green_mask = cv.inRange(hsv, lower_green, upper_green)
+
+    # Bitwise-AND mask and original image
+    red_result = cv.bitwise_and(colored_board, colored_board, mask= red_mask)
+    blue_result = cv.bitwise_and(colored_board, colored_board, mask= blue_mask)
+    green_result = cv.bitwise_and(colored_board, colored_board, mask= green_mask)
+
+    # display the mask and masked image
+    # cv.imshow('Mask', green_mask)
+    # cv.waitKey(0)
+    # cv.imshow('Masked Image', green_result)
+    # cv.waitKey(0)
+    # cv.imshow('colored_board', colored_board)
+    # cv.waitKey(0)
+    # cv.destroyAllWindows()
+
+    return red_result, blue_result, green_result
 
 def detect_circle(board_image):
     """
@@ -68,14 +156,13 @@ def detect_circle(board_image):
 
         for pt in detected_circles[0, :]:
             a, b, r = pt[0], pt[1], pt[2]
-
-            # need to determine the color of the circle
-
             # Draw the circumference of the circle
-            cv.circle(board_image, (a, b), r, (0, 255, 0), 2)
+            # syntax : 
+            # cv2.circle(image, center_coordinates, radius, color, thickness)
+            cv.circle(board_image, (a, b), r, (255, 0, 0), 2)
             # Draw a small circle (of radius 1) to show the center.
-            cv.circle(board_image, (a, b), 1, (0, 0, 255), 3)
-
+            cv.circle(board_image, (a, b), 1, (255, 0, 0), 3)
+            print(a, b, r)
             cv.imshow("Detected Circle", board_image)
             cv.waitKey(0)
             cv.destroyAllWindows()
@@ -111,9 +198,18 @@ def map_coordinates(board_coordinate):
 
 if __name__ == "__main__":
     path = "/Users/patrickdharma/Desktop/programming/openCV/moonboard_DatasetProject/Resources/Photos/moonboard.PNG"
-    img, gray_blurred = read_image(path)
-    board_part, text_part = crop_img(gray_blurred)
-    detect_circle(board_part)
+    img = read_image(path)
+    colored_board, colored_text = crop_img(img)
+    red_masked, blue_masked, green_masked =  remove_background_noise(colored_board)
+    # blur each masks
+    red_blurred = blur_image(red_masked)
+    blue_blurred = blur_image(blue_masked)
+    green_blurred = blur_image(green_masked)
+    
+
+    detect_red = detect_circle(red_blurred)
+    detect_blue = detect_circle(blue_blurred)
+    detect_green = detect_circle(green_blurred)
     
     # call the mouse_callback function with the cropped image part
     # mouse_callback("measurer", board_part)
